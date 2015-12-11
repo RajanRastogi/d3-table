76e5f9126d8556eab332301b533bd724f72ba3ca
Rajan Rastogi <rajan.rastogi@datarpm.com>
2015-12-11 17:08:28 +0530

    chore(tests): include test outline
    Signed-off-by: Rajan Rastogi <rajan.rastogi@datarpm.com>

---
+++ tests/spec/ajax.js
@@ -0,0 +1,17 @@
+describe("D3Table", function(){
+	it("can process json data", function(){
+
+	})
+
+	it("can process csv data", function(){
+
+	})
+
+	it("can process tsv data", function(){
+
+	})
+
+	it("allows custom data transformers", function(){
+		
+	})
+})
---
+++ tests/spec/columnOptions.js
@@ -0,0 +1,19 @@
+describe("D3Table's ColumnOptions ", function(){
+
+	it("let you customize the column title", function(){
+
+	})
+
+	it("let you compute column values", function(){
+
+	})
+
+	it("let you combine columns", function(){
+
+	})
+
+	it("let you insert custom html", function(){
+
+	})
+
+});
